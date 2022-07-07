3ac466f7545056ab67df78d784995e1db1dec3d2
HamsterStudent <hwanyamyu@gmail.com>
2022-07-07 23:20:45 +0900

    20220707

--- javascript/app.js
+++ javascript/app.js
@@ -1,16 +1,25 @@
-const age = parseInt(prompt("How old are you?"))
-//자바스크립트의 실행을 일시적으로 멈춤. 잘안씀 못생기고 별로 안좋아서...
+// const age = parseInt(prompt("How old are you?"))
+// //자바스크립트의 실행을 일시적으로 멈춤. 잘안씀 못생기고 별로 안좋아서...
 
-console.log(isNaN(age))
+// console.log(isNaN(age))
 
-if(isNaN(age)){
-    console.log("햄스터")
-}else if(age < 18){
-    console.log("진짠가요")
-}else {
-    console.log("환영합니다")
-}
+// if(isNaN(age)){
+//     console.log("햄스터")
+// }else if(age < 18){
+//     console.log("진짠가요")
+// }else {
+//     console.log("환영합니다")
+// }
 
-if((a && b) || (c && d)){
-    console.log("이렇게도 할수있구나~")
-}
+// if((a && b) || (c && d)){
+//     console.log("이렇게도 할수있구나~")
+// }
+
+// console.dir('hello')
+// 해당 객체에 들어 있는 것들을 보여줌
+// 이름 앞에 on이 붙어있으면 이벤트입니다~
+
+// title.addEventListener("click", clickEvent)
+// title.onclick = clickEvent;
+// 위와 아래는 같은 이벤트를 발생시킴. onmouseenter, onmouseleave등도 마찬가지
+// addEventListener형식으로 쓰면 remove도 가능하기 때문에 좋음
