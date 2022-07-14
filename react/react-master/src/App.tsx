6bc9b292ee705c3bcda36036d26dbd988e55741f
HamsterStudent <hwanyamyu@gmail.com>
2022-07-15 00:37:16 +0900

    20220715

--- react/react-master/src/App.tsx
+++ react/react-master/src/App.tsx
@@ -1,24 +1,6 @@
-import React from "react";
-import styled from "styled-components";
+import Router from "./Router";
 
-const Container = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-`;
-
-const H1 = styled.h1`
-color: ${(props) => props.theme.textColor};
-`;
-
-function App() {
-
-  return (
-    <div>
-      <Container>
-        <H1>protexted</H1>
-      </Container>
-      
-    </div>
-  )
-};
-
+function App(){
+  return <Router />
+}
 export default App;
--- react/react-master/src/theme.ts
+++ react/react-master/src/theme.ts
@@ -1,13 +1,13 @@
 import { DefaultTheme } from "styled-components";
 
-export const lightTheme:DefaultTheme = {
+export const theme:DefaultTheme = {
     bgColor:"white",
     textColor: "black",
     btnColor: "tomato"
 }
 
-export const darkTheme:DefaultTheme = {
-    bgColor:"black",
-    textColor: "white",
-    btnColor: "teal"
-}
+// export const darkTheme:DefaultTheme = {
+//     bgColor:"black",
+//     textColor: "white",
+//     btnColor: "teal"
+// }
